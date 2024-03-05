7b5d88bc209046e0677de5a9d4084c65b30849ef
Zach Cheatham <zachcheatham@gmail.com>
2024-03-05 16:34:15 -0600

    Correctly handle challenge responses from servers...

--- src/source_query.rs
+++ src/source_query.rs
@@ -25,6 +25,7 @@ pub struct A2SInfoResult {
 const A2SINFO_PACKET: &[u8] = b"\xFF\xFF\xFF\xFF\x54Source Engine Query\x00";
 const PACKET_HEADER: i32 = -1;
 const S2AINFO_HEADER: u8 = b'\x49';
+const CHALLENGE_HEADER: u8 = b'\x41';
 
 impl SourceQuery {
 
@@ -43,7 +44,7 @@ impl SourceQuery {
 
     pub fn query_a2s_info(&self) -> Result<A2SInfoResult, Box<dyn Error>> {
         let socket = UdpSocket::bind("0.0.0.0:0")?;
-        let mut buf = [0; 1024];
+        let mut buf = [0; 200];
 
         // let debug_str = A2SINFO_PACKET.iter().map(|b| format!("{:02X}", b)).collect::<String>();
 
@@ -56,45 +57,70 @@ impl SourceQuery {
         if src.to_string() != self.full_host {
             return Err("Invalid host responded.".into());
         }
+
+        // println!("{:x?}", buf);
         
-        let mut cursor = Cursor::new(&buf[..]);
-        
-        if PACKET_HEADER != read_long(&mut cursor) {
+        if PACKET_HEADER != i32::from_le_bytes(buf[0..4].try_into().unwrap()) {
             return Err("Invalid packet header in response.".into());
         }
 
-        if S2AINFO_HEADER != read_byte(&mut cursor) {
-            return Err("Invalid S2A_INFO packet header in response.".into());
+        if CHALLENGE_HEADER == buf[4] {
+            let mut challenge_reply = A2SINFO_PACKET.to_vec();
+            challenge_reply.extend_from_slice(&buf[5..9]);
+
+            // println!("{:x?}", challenge_reply);
+
+            socket.send_to(&challenge_reply, &self.full_host).unwrap();
+            socket.recv_from(&mut buf).unwrap();
+
+            // println!("{:x?}", buf);
+
+            if PACKET_HEADER != i32::from_le_bytes(buf[0..4].try_into().unwrap()) {
+                return Err("Invalid packet header in response after challenge.".into());
+            }
+
+            Ok(handle_a2s_response(&buf, ping))
         }
-
-        let server_name = read_string(&mut cursor);
-        let server_map = read_string(&mut cursor);
-        let server_mod = read_string(&mut cursor);
-        let server_game = read_string(&mut cursor);
-        let game_id = read_short(&mut cursor);
-        let num_players = read_byte(&mut cursor);
-        let max_players = read_byte(&mut cursor);
-        let bot_players = read_byte(&mut cursor);
-
-        // println!("{} (map: {}) (mod: {}) (game: {}) (game_id: {}) (players: {}) (max_players: {}) (bots: {})",
-        //     server_name, server_map, server_mod, server_game, game_id, num_players, max_players, bot_players);
-
-
-        let result = A2SInfoResult {
-            ping,
-            server_name,
-            map: server_map,
-            folder: server_mod,
-            game: server_game,
-            game_id,
-            num_players,
-            num_bots: bot_players,
-            max_players
-        };
-
-        Ok(result)
+        else
+        {
+            Ok(handle_a2s_response(&buf, ping))
+        }
     }
     
+}
+
+fn handle_a2s_response(buf: &[u8], ping: u128) -> A2SInfoResult {
+
+    let mut cursor = Cursor::new(buf);
+    cursor.set_position(4);
+
+    if S2AINFO_HEADER != read_byte(&mut cursor) {
+        println!("Invalid S2A_INFO packet header in response.");
+    }
+
+    let server_name = read_string(&mut cursor);
+    let server_map = read_string(&mut cursor);
+    let server_mod = read_string(&mut cursor);
+    let server_game = read_string(&mut cursor);
+    let game_id = read_short(&mut cursor);
+    let num_players = read_byte(&mut cursor);
+    let max_players = read_byte(&mut cursor);
+    let bot_players = read_byte(&mut cursor);
+
+    // println!("{} (map: {}) (mod: {}) (game: {}) (game_id: {}) (players: {}) (max_players: {}) (bots: {})",
+    //     server_name, server_map, server_mod, server_game, game_id, num_players, max_players, bot_players);
+
+    A2SInfoResult {
+        ping,
+        server_name,
+        map: server_map,
+        folder: server_mod,
+        game: server_game,
+        game_id,
+        num_players,
+        num_bots: bot_players,
+        max_players
+    }
 }
 
 fn read_string(cursor: &mut Cursor<&[u8]>) -> String {
